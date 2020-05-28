9e3af83759785dfb247e854d3109d86252c3bb4f
Ankur Baranwal <ankur.baranwal@deltaschool.in>
2020-05-28 23:54:11 +0530

    Social login implemented.

---
+++ FirebasePracticeDemo/GoogleManager.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleManager.swift
+//  FirebasePracticeDemo
+//
+//  Created by Ankur Baranwal on 27/05/20.
+//  Copyright © 2020 Ankur Baranwal. All rights reserved.
+//
+
+import Foundation
